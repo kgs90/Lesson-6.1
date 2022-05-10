cdc245ee944cdfd768e952079900827403550b61
kgs90 <kgs90@inbox.ru>
2022-05-10 10:12:11 +0300

    zadacha2

---
+++ Task2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите четыре значения через пробел: ");
+
+double[] mas = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+double x = (mas[2] - mas[0]) / (mas[1] - mas[3]); 
+double y = mas[1] * x + mas[0]; 
+
+Console.WriteLine("Точка пересечения двух прямых: (" + x + ", " + y + ")");
