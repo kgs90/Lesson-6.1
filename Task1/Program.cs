8c60694a8b81111998d8f8ed00ae00fb1533b477
kgs90 <kgs90@inbox.ru>
2022-05-10 10:05:42 +0300

    zadacha1

---
+++ Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿int i = 0;
+int[] array = new int[1000];
+
+Console.WriteLine("Введите M чисел с клавиатуры через пробел: ");
+
+int[] mas = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+for (int j = 0; j < mas.Length; j++) if (mas[j] > 0) i++;
+
+Console.WriteLine($"Количество положительных чисел = {i}");
